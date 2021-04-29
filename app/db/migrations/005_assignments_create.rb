2ea1e6cb813dedfca47f8038ff04256f36a35b6c
chinyakao <chinyariley@gmail.com>
2021-04-29 14:53:37 +0800

    change folder to course

--- app/db/migrations/005_assignments_create.rb
+++ app/db/migrations/005_assignments_create.rb
@@ -6,7 +6,7 @@ Sequel.migration do
   change do
     create_table(:assignments) do
       primary_key :id
-      foreign_key :folder_id, table: :folders
+      foreign_key :course_id, table: :courses
 
       String :name, unique: true, null: false
       String :content, unique: true, null: false
@@ -14,7 +14,7 @@ Sequel.migration do
       DateTime :created_at
       DateTime :updated_at
 
-      unique %I[folder_id name]
+      unique %I[course_id name]
     end
   end
 end
