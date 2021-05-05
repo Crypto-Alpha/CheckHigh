8432a5236d80f09ae79c81bde0224cf4f2209100
rainyjonne <rainyjonne@gmail.com>
2021-05-05 22:01:33 +0800

    Update assignment migration for db encryption

--- app/db/migrations/005_assignments_create.rb
+++ app/db/migrations/005_assignments_create.rb
@@ -8,13 +8,13 @@ Sequel.migration do
       uuid :id, primary_key: true
       foreign_key :course_id, table: :courses
 
-      String :assignment_name, null: false, default: ''
-      String :content, null: false, default: ''
+      String :assignment_name_secure, null: false, default: ''
+      String :content_secure, null: false, default: ''
 
       DateTime :created_at
       DateTime :updated_at
 
-      unique %I[course_id assignment_name]
+      unique %I[course_id assignment_name_secure]
     end
   end
 end
