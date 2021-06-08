88ec9b96e09b37807e779ae207daed2a6725bb44
chinyakao <riley.kao@iss.nthu.edu.tw>
2021-06-08 23:18:52 +0800

    course policy

--- app/controllers/courses.rb
+++ app/controllers/courses.rb
@@ -1,80 +1,94 @@
 # frozen_string_literal: true
 
-require 'roda'
 require_relative './app'
 
+# rubocop:disable Metrics/BlockLength
 module CheckHigh
   # Web controller for CheckHigh API
   class Api < Roda
     route('courses') do |routing|
-      # GET api/v1/courses
-      routing.is do
-        routing.get do
-          account = Account.first(username: @auth_account['username'])
-          courses = account.courses
-          JSON.pretty_generate(data: courses)
-        rescue StandardError
-          routing.halt 404, { message: 'Could not find any course' }.to_json
-        end
+      unauthorized_message = { message: 'Unauthorized Request' }.to_json
+      routing.halt(403, unauthorized_message) unless @auth_account
 
-        # POST api/v1/courses/
-        routing.post do
-          new_data = JSON.parse(routing.body.read)
-          new_course = Course.new(new_data)
-          raise('Could not save course') unless new_course.save
-
-          response.status = 201
-          response['Location'] = "#{@api_root}/courses"
-          { message: 'Course saved', data: new_course }.to_json
-        rescue Sequel::MassAssignmentRestriction
-          Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
-          routing.halt 400, { message: 'Illegal Attributes' }.to_json
-        rescue StandardError => e
-          routing.halt 400, { message: e.message }.to_json
-        end
-      end
-
-      # GET api/v1/courses/[course_id]
-      routing.get String do |course_id|
-        course = JSON.parse(Course.find(id: course_id).to_json)['data']['attributes']
-        output = { data: course }
-        JSON.pretty_generate(output)
-      rescue StandardError
-        routing.halt 404, { message: 'Could not find course details' }.to_json
-      end
+      @crs_route = "#{@api_root}/courses"
 
       routing.on String do |course_id|
-        routing.on 'assignments' do
+        @req_course = Course.first(id: course_id)
+
+        routing.on('assignments') do
+          # TODO_0608: need to check the logic of AssignmentPolicy::CourseScope
           # GET api/v1/courses/[course_id]/assignments
+          # return specific course's assignments
           routing.get do
-            # account = Account.first(username: @auth_account['username'])
-            course = Course.find(id: course_id)
-            assignments = course.assignments
+            assignments = AssignmentPolicy::CourseScope.new(@req_course).viewable
             JSON.pretty_generate(data: assignments)
           rescue StandardError
-            routing.halt 404, { message: 'Could not find any related assignments for this course' }.to_json
+            routing.halt 403, { message: 'Could not find any related assignments for this course' }.to_json
           end
 
           # POST api/v1/courses/[course_id]/assignments
           # create new assignments in specific course
           routing.post do
-            new_data = JSON.parse(routing.body.read)
             new_assignment = CreateAssiForCourse.call(
-              course_id: course_id, assignment_data: new_data
+              account: @auth_account,
+              course: @req_course,
+              assignment_data: JSON.parse(routing.body.read)
             )
-            raise('Could not save new assignment for this course') unless new_assignment.save
 
             response.status = 201
-            response['Location'] = "#{@api_root}/courses/#{course_id}/assignments"
-            { message: 'Course related assignment saved', data: new_assignment }.to_json
-          rescue Sequel::MassAssignmentRestriction
-            Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
-            routing.halt 400, { message: 'Illegal Attributes' }.to_json
-          rescue StandardError => e
+            response['Location'] = "#{@assi_route}/#{new_assignment.id}"
+            { message: 'Assignment saved', data: new_assignment }.to_json
+          rescue CreateAssiForCourse::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue CreateAssiForCourse::IllegalRequestError => e
             routing.halt 400, { message: e.message }.to_json
+          rescue StandardError => e
+            puts "CREATE_ASSIGNMENT_FOR_COURSE_ERROR: #{e.inspect}"
+            routing.halt 500, { message: 'API server error' }.to_json
           end
+        end
+
+        # GET api/v1/courses/[course_id]
+        # return a specific course
+        routing.get do
+          course = GetCourseQuery.call( account: @auth_account, course: @req_course )
+          { data: course }.to_json
+        rescue GetCourseQuery::ForbiddenError => e
+          routing.halt 403, { message: e.message }.to_json
+        rescue GetCourseQuery::NotFoundError => e
+          routing.halt 404, { message: e.message }.to_json
+        rescue StandardError => e
+          puts "FIND COURSE ERROR: #{e.inspect}"
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
+      end
+
+      routing.is do
+        # GET api/v1/courses
+        # return course list
+        routing.get do
+          courses = CoursePolicy::AccountScope.new(@auth_account).viewable
+          JSON.pretty_generate(data: courses)
+        rescue StandardError
+          routing.halt 403, { message: 'Could not find any courses' }.to_json
+        end
+
+        # POST api/v1/courses
+        # create a course
+        routing.post do
+          new_data = JSON.parse(routing.body.read)
+          new_course = @auth_account.add_owned_course(new_data)
+          
+          response.status = 201
+          response['Location'] = "#{@crs_route}/#{new_course.id}"
+          { message: 'Course saved', data: new_course }.to_json
+        rescue Sequel::MassAssignmentRestriction
+          routing.halt 400, { message: 'Illegal Request' }.to_json
+        rescue StandardError
+          routing.halt 500, { message: 'API server error' }.to_json
         end
       end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength
