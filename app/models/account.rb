67513771e471369a77bbd947ee98331dc6752140
chinyakao <riley.kao@iss.nthu.edu.tw>
2021-06-18 03:50:15 +0800

    github sso

--- app/models/account.rb
+++ app/models/account.rb
@@ -29,6 +29,11 @@ module CheckHigh
 
     plugin :timestamps, update_on_create: true
 
+    def self.create_github_account(github_account)
+      create(username: github_account[:username],
+             email: github_account[:email])
+    end
+
     def courses
       owned_courses
     end
